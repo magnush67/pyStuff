9912605394f225c7e9616f308df09553227ad84a
Magnus <magnus.hellertz@tekenable.com>
2025-06-01 12:13:35 +0100

    Updates

--- src/fruits.py
+++ src/fruits.py
@@ -1,9 +1,9 @@
-item = 'Bananas'
-item2 = 'Apples'
-item_name = 'Juice'
+item_b = 'bananas'
+item_a = 'apples'
+item_j = 'juice'
 int1 = 123
-int2 = 123
+int2 = 321
 
-print("I like to eat "+item2)
+print("I like to eat "+item_b+" and drink "+item_j)
 
 print (int1 + int2)
